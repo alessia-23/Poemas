1a80cd6b090643cfb2dd5fddb1f5ae8b6c551abb
Alessia Perez <alessia.perez@epn.edu.ec>
2026-05-15 16:45:24 -0500

    Actualizaxiones

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,27 +9,16 @@ export const routes: Routes = [
   {
     path: 'videojuegos',
     loadComponent: () =>
-      import('./pages/videojuegos/videojuegos.page')
-        .then(m => m.VideojuegosPage)
-  },
-  {
-    path: 'videojuegos-form',
-    loadComponent: () =>
-      import('./pages/videojuego-form/videojuego-form.page')
-        .then(m => m.VideojuegoFormPage)
-  },
-  {
-    path: 'videojuegos-form/:id',
-    loadComponent: () =>
-      import('./pages/videojuego-form/videojuego-form.page')
-        .then(m => m.VideojuegoFormPage)
-  },
-  {
-    path: 'videojuegos',
-    loadComponent: () => import('./pages/videojuegos/videojuegos.page').then( m => m.VideojuegosPage)
+      import('./pages/videojuegos/videojuegos.page').then(m => m.VideojuegosPage)
   },
   {
     path: 'videojuego-form',
-    loadComponent: () => import('./pages/videojuego-form/videojuego-form.page').then( m => m.VideojuegoFormPage)
+    loadComponent: () =>
+      import('./pages/videojuego-form/videojuego-form.page').then(m => m.VideojuegoFormPage)
+  },
+  {
+    path: 'videojuego-form/:id',
+    loadComponent: () =>
+      import('./pages/videojuego-form/videojuego-form.page').then(m => m.VideojuegoFormPage)
   }
 ];
--- src/app/pages/videojuegos/videojuegos.page.ts
+++ src/app/pages/videojuegos/videojuegos.page.ts
@@ -1,9 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import {
-  IonHeader, IonToolbar, IonTitle, IonContent,
-  IonList, IonItem, IonLabel, IonButton, IonIcon,
-  IonFab, IonFabButton
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButton,
+  IonFab,
+  IonFabButton,
+  IonCard,
+  IonCardContent,
+  IonImg,
+  IonChip,
+  IonList,
+  IonItem,
+  IonLabel
 } from '@ionic/angular/standalone';
+
 import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { VideojuegosService, Videojuego } from '../../services/videojuegos';
@@ -11,20 +23,33 @@ import { VideojuegosService, Videojuego } from '../../services/videojuegos';
 @Component({
   selector: 'app-videojuegos',
   templateUrl: './videojuegos.page.html',
+  styleUrls: ['./videojuegos.page.scss'],
   standalone: true,
+
   imports: [
     CommonModule,
     RouterLink,
-    IonHeader, IonToolbar, IonTitle, IonContent,
-    IonList, IonItem, IonLabel, IonButton,
-    IonFab, IonFabButton
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonButton,
+    IonFab,
+    IonFabButton,
+    IonCard,
+    IonCardContent,
+    IonImg,
+    IonChip,
+    IonList,
+    IonItem,
+    IonLabel
   ]
 })
 export class VideojuegosPage implements OnInit {
 
   videojuegos: Videojuego[] = [];
 
-  constructor(private videojuegosService: VideojuegosService) {}
+  constructor(private videojuegosService: VideojuegosService) { }
 
   ngOnInit() {
     this.cargar();
